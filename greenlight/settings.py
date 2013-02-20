0a044a4d963cc882b4eeba1249957dd72183534c
Fletcher Tomalty <fletcher@tomalty.com>
2013-02-20 12:48:01 -0500

    Initial commit with blank Django app.

---
+++ greenlight/settings.py
@@ -0,0 +1,50 @@
+# Django settings for greenlight project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+	# ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+TIME_ZONE = 'America/Montreal'
+
+USE_I18N = False
+
+SECRET_KEY = '9&amp;@r*3hth4m=ml5t5tme0*(9^x@2xqo-ua^s+wg_ws(-^4-7@v'
+
+MIDDLEWARE_CLASSES = (
+	'django.middleware.common.CommonMiddleware',
+)
+
+ROOT_URLCONF = 'greenlight.urls'
+
+WSGI_APPLICATION = 'greenlight.wsgi.application'
+
+INSTALLED_APPS = ()
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True,
+		},
+	}
+}
---
+++ greenlight/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'greenlight.views.home', name='home'),
+    # url(r'^greenlight/', include('greenlight.foo.urls')),
+)
